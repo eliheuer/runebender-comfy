8acee79ec18d01485bb2a6f3ad49fbbdfdba2a76
Eli H <elih@protonmail.com>
2026-06-08 16:57:07 -0700

    Update bundle for trace divergence diagnostics

--- web/src/extension.ts
+++ web/src/extension.ts
@@ -11,7 +11,7 @@ import { runebenderHostKey, type WorkspaceChoice } from "./host/runebenderHost";
 import { comfyHost } from "./hosts/comfy/comfyHost";
 import Runebender from "./Runebender.vue";
 
-const RUNEBENDER_BUNDLE_FINGERPRINT = "rb-bundle-2026-06-08-extrema-visible-oversnap-clean150";
+const RUNEBENDER_BUNDLE_FINGERPRINT = "rb-bundle-2026-06-08-final-divergence-diagnostic";
 
 // Mirror our own console output to the ComfyUI terminal through the
 // injected Comfy host. Filters to messages prefixed with
